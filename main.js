f7a99a5a382b3f8abd25dd2b67008f7c916cb9e6
nobmurakita <nobmurakita@users.noreply.github.com>
2024-02-25 19:28:59 +0900

    build

--- main.js
+++ main.js
@@ -1,11 +1,84 @@
 
-const { app, BrowserWindow } = require('electron/main')
+const fs = require('fs');
+const { app, Menu, dialog, BrowserWindow } = require('electron/main')
+const Store = require('electron-store');
 const { createServer } = require('./server.js');
-require('dotenv').config();
+
+const store = new Store();
+let songsDir = null;
+let win = null;
+
+const loadMainWindow = async (songsDir) => {
+  const resourcesPath = app.isPackaged ? process.resourcesPath : '.';
+  const port = await createServer(resourcesPath, songsDir);
+  win.loadURL(`http://127.0.0.1:${port}`);
+};
+
+const openSongsDir = async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog(win, {
+    title: 'Open Directory',
+    defaultPath: songsDir || app.getPath('home'),
+    properties: ['openDirectory'],
+  });
+  if (canceled) {
+    return;
+  }
+  songsDir = filePaths[0];
+  store.set('songsDir', songsDir);
+  loadMainWindow(songsDir);
+};
+
+const createMenu = () => {
+  Menu.setApplicationMenu(Menu.buildFromTemplate([
+    {
+      label: app.getName(),
+      submenu: [
+        {
+          label: 'Quit',
+          role: 'quite',
+        },
+      ],
+    },
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open...',
+          accelerator: 'CmdOrCtrl+O',
+          click: () => openSongsDir(),
+        },
+        {
+          label: 'Close Window',
+          role: 'close',
+        }
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          role: 'reload',
+        },
+        {
+          label: 'Toggle Developer Tools',
+          role: 'toggleDevTools',
+        },
+      ],
+    },
+  ]));
+};
 
 app.whenReady().then(async () => {
-    const songsDir = process.env.SONGS_DIR || './songs/';
-    const port = await createServer(songsDir);
-    const win = new BrowserWindow();
-    win.loadURL(`http://127.0.0.1:${port}`);
+  songsDir = store.get('songsDir');
+  if (songsDir) {
+    if (!fs.existsSync(songsDir) || !fs.lstatSync(songsDir).isDirectory()) {
+      songsDir = null;
+      store.set('songsDir', null);
+    }
+  }
+
+  createMenu();
+  win = new BrowserWindow();
+  loadMainWindow(songsDir);
 });
--- server.js
+++ server.js
@@ -3,54 +3,66 @@ const { serve } = require('@hono/node-server');
 const { serveStatic } = require('@hono/node-server/serve-static');
 
 const { readFile } = require('fs').promises;
-const { relative } = require('path');
+const { resolve, relative } = require('path');
 const { glob } = require('glob');
 const { renderFile } = require('ejs');
 
-exports.createServer = async (songsDir) => {
-    const app = new Hono();
+let server = null;
 
-    app.get('/', async (c) => {
-        const rlrrFiles = await glob(`${songsDir}/*/*.rlrr`);
-    
-        const songs = await Promise.all(rlrrFiles.map(async (rlrrFile) => {
-            const rlrr = JSON.parse(await readFile(rlrrFile));
-            const metaData = rlrr.recordingMetadata;
-            const m = rlrrFile.match(/^.*_(Easy|Medium|Hard|Expert)\.rlrr$/);
-            const level = m[1];
-            const levelNum = ['Easy', 'Medium', 'Hard', 'Expert'].indexOf(level);
-            return {
-                artist: metaData.artist,
-                title: metaData.title,
-                level: level,
-                levelNum: levelNum,
-                playerLink: `/player?rlrr=${encodeURI(relative(songsDir, rlrrFile))}`,
-            };
-        }));
-    
-        songs.sort((a, b) => {
-            aa = [a.artist, a.title, a.levelNum].join('|').toUpperCase();
-            bb = [b.artist, b.title, b.levelNum].join('|').toUpperCase();
-            return aa == bb ? 0 : (aa < bb ? -1 : 1);
-        });
-    
-        return c.html(renderFile('index.ejs', { songs }));
-    })
-    
-    app.use('/player/*', serveStatic({ root: '.' }));
-    
+exports.createServer = async (resourcesPath, songsDir) => {
+  if (server) {
+    server.close();
+    server = null;
+  }
+
+  const app = new Hono();
+
+  app.get('/', async (c) => {
+    let songs = [];
+    if (songsDir) {
+      const rlrrFiles = await glob(`${songsDir}/*/*.rlrr`);
+
+      songs = await Promise.all(rlrrFiles.map(async (rlrrFile) => {
+        const rlrr = JSON.parse(await readFile(rlrrFile));
+        const metaData = rlrr.recordingMetadata;
+        const m = rlrrFile.match(/^.*_(Easy|Medium|Hard|Expert)\.rlrr$/);
+        const level = m[1];
+        const levelNum = ['Easy', 'Medium', 'Hard', 'Expert'].indexOf(level);
+        return {
+          artist: metaData.artist,
+          title: metaData.title,
+          level: level,
+          levelNum: levelNum,
+          playerLink: `/player?rlrr=${encodeURI(relative(songsDir, rlrrFile))}`,
+        };
+      }));
+
+      songs.sort((a, b) => {
+        aa = [a.artist, a.title, a.levelNum].join('|').toUpperCase();
+        bb = [b.artist, b.title, b.levelNum].join('|').toUpperCase();
+        return aa == bb ? 0 : (aa < bb ? -1 : 1);
+      });
+    }
+
+    return c.html(renderFile(resolve(resourcesPath, 'index.ejs'), { songsDir, songs }));
+  })
+
+  app.use('/player/*', serveStatic({ root: resourcesPath }));
+
+  if (songsDir) {
     app.use('/songs/*', serveStatic({
-        root: relative('.', songsDir),
-        rewriteRequestPath: (path) => path.replace(/^\/songs/, ''),
+      root: relative('.', songsDir),
+      rewriteRequestPath: (path) => path.replace(/^\/songs/, ''),
     }));
-    
-    return new Promise(resolve => {
-        serve({
-            fetch: app.fetch,
-            hostname: '127.0.0.1',
-            port: 0,
-        }, info => {
-            resolve(info.port);
-        });    
+  }
+
+  return new Promise(resolve => {
+    server = serve({
+      fetch: app.fetch,
+      hostname: '127.0.0.1',
+      port: 0,
+    }, info => {
+      resolve(info.port);
     });
+  });
 };
